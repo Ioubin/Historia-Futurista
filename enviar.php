7ba6c0ac4200ed5cc880b92f62d0e6592ecda6c4
Ioubin <oubina.ignacio@gmail.com>
2025-05-13 13:06:37 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Enviar</title>
+</head>
+
+<body>
+<?php
+
+//se arma el array POST
+$nombre=$_POST['nombre'];
+$email=$_POST['email'];
+$asunto=$_POST['asunto'];
+$mensaje=$_POST['mensaje'];
+
+$destino="mail@mail.com,mail2@mail.com";
+$asunto="Contacto desde el sitio";
+$mensaje="Nombre: ".$nombre." Email: ".$email." Asunto: ".$asunto." Mensaje: ".$mensaje;
+
+$header="From: ".$nombre."<".$email.">";
+
+@$enviado = mail($destino,$asunto,$mensaje,$header);
+
+if($enviado == true){
+	echo "Su correo ha sido enviado.";
+}else{
+	echo "Hubo un error en el envio del mail.";
+}
+
+$conexion = mysqli_connect("localhost","root","", "contactos") or die('No se pudo conectar al servidor');
+
+
+// $consulta=mysqli_query($conexion, "INSERT INTO contactos VALUES ('','$nombre','$email','$localidad','$comentario')");
+
+$consulta = mysqli_query($conexion, "INSERT INTO contactos (nombre,email,asunto,mensaje)VALUES ( '$nombre','$email','$asunto','$mensaje')") or die(mysqli_error($conexion));
+
+?>
+</body>
+</html>
